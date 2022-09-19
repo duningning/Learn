eeb80affc390534f5e98fb882db81566024b1f26
duningning <duning132@163.com>
2022-09-19 16:33:57 +0800

    渲染模板

--- Flask/app3_渲染模板.py
+++ Flask/app3_渲染模板.py
@@ -11,7 +11,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index21.html')
+    t_int = 18
+    t_str = 'curry'
+    t_list = [2,5,4,3,2]
+    t_dict ={'name': 'durant', 'age': 28}
+    #render_template方法：渲染模板
+    #参数1： 模板名称 参数n:传到模板里的数据
+    return render_template('index3.html',
+                           my_int = t_int,
+                           my_str = t_str,
+                           my_list = t_list,
+                           my_dict = t_dict)
+
 
 if __name__ == "__main__":
     app.run(debug=True,threaded =True)
