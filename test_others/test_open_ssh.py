da5106f9182966934c9bd7578b73c0d85854ad6b
duningning <duning132@163.com>
2022-10-21 12:19:32 +0800

    jsx语法规则

--- test_others/test_open_ssh.py
+++ test_others/test_open_ssh.py
@@ -9,7 +9,7 @@ import pytest
 
 
 def test_open_ssh():
-    ip = '172.17.21.8'
+    ip = '172.17.21.2'
     r = requests.get('http://' + ip + ':9000/sys/deviceinfo').json()
     key = 'deepglint' + r['data']['id']
     token = hashlib.md5(key.encode('utf-8')).hexdigest()
